7a52d02094255a02e4d32d3225d01e3e83a6d897
Buzivskoy Vyacheslav <vyacheslav.buzivskoy@ukmig.com.ua>
2020-03-10 17:11:57 +0200

    Add -f option help description in gendiff.js

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -5,6 +5,9 @@ import { Command } from 'commander';
 const program = new Command();
 program
   .version('0.0.1')
-  .description('Compares two configuration files and shows a difference.');
+  .usage('gendiff [options] <firstConfig> <secondConfig>')
+  .description('Compares two configuration files and shows a difference.')
+  .arguments('[firstConfig] [secondConfig]')
+  .option('-f, --format [type]', 'output format');
 
 program.parse(process.argv);
