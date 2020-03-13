13187aeca104d907b7bbc2867f4ca6f33b8625a5
Buzivskoy Vyacheslav <vyacheslav.buzivskoy@ukmig.com.ua>
2020-03-13 13:29:16 +0200

    Add tests of genDiff function for plan json files

---
+++ __tests__/gendiff.test.js
@@ -0,0 +1,22 @@
+import genDiff from '../src';
+
+test('Plain JSON genDiff test', () => {
+  const pathToBeforeFile = '__tests__/before.json';
+  const pathToAfterFile = '__tests__/after.json';
+  const diff = `{
+    host: hexlet.io
+  + timeout: 20
+  - timeout: 50
+  - proxy: 123.234.53.22
+  + verbose: true
+  - follow: false
+}`;
+
+  expect(new Set(genDiff(pathToBeforeFile, pathToAfterFile).split('\n'))).toEqual(new Set(diff.split('\n')));
+  expect((() => {
+    const diff = genDiff(pathToBeforeFile, pathToAfterFile).split('\n');
+    const plusTimeoutKeyIndex = diff.indexOf('  + timeout: 20');
+    const minusTimeoutKeyIndex = diff.indexOf('  - timeout: 50');
+    return Math.abs(plusTimeoutKeyIndex - minusTimeoutKeyIndex);
+  })()).toEqual(1);
+});
