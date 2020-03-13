1c2f7de984da4602b7e2388b96bd7c8e8828ff14
Buzivskoy Vyacheslav <vyacheslav.buzivskoy@ukmig.com.ua>
2020-03-13 17:38:37 +0200

    Add plain YAML tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -20,3 +20,25 @@ test('Plain JSON genDiff test', () => {
     return Math.abs(plusTimeoutKeyIndex - minusTimeoutKeyIndex);
   })()).toEqual(1);
 });
+
+
+test('Plain YAML genDiff test', () => {
+  const pathToBeforeFile = '__tests__/fixtures/before.yaml';
+  const pathToAfterFile = '__tests__/fixtures/after.yaml';
+  const diff = `{
+    host: hexlet.io
+  + timeout: 20
+  - timeout: 50
+  - proxy: 123.234.53.22
+  + verbose: true
+  - follow: false
+}`;
+
+  expect(new Set(genDiff(pathToBeforeFile, pathToAfterFile).split('\n'))).toEqual(new Set(diff.split('\n')));
+  expect((() => {
+    const diff = genDiff(pathToBeforeFile, pathToAfterFile).split('\n');
+    const plusTimeoutKeyIndex = diff.indexOf('  + timeout: 20');
+    const minusTimeoutKeyIndex = diff.indexOf('  - timeout: 50');
+    return Math.abs(plusTimeoutKeyIndex - minusTimeoutKeyIndex);
+  })()).toEqual(1);
+});
