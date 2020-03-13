c1f48f822aff4e0838e62fac0b051fa11694ae04
Buzivskoy Vyacheslav <vyacheslav.buzivskoy@ukmig.com.ua>
2020-03-13 13:28:31 +0200

    Add call of genDiff function to bin file

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,13 +1,20 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander';
+import genDiff from '../';
+
 
 const program = new Command();
 program
   .version('0.0.1')
   .usage('gendiff [options] <firstConfig> <secondConfig>')
   .description('Compares two configuration files and shows a difference.')
-  .arguments('[firstConfig] [secondConfig]')
-  .option('-f, --format [type]', 'output format');
+  .arguments('<firstConfig> <secondConfig>')
+  .option('-f, --format [type]', 'output format')
+  .action((firstConfig, secondConfig) => {
+    const beforeConfig = firstConfig;
+    const afterConfig = secondConfig;
+    console.log(genDiff(beforeConfig, afterConfig));
+  });
 
 program.parse(process.argv);
