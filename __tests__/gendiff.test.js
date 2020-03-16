f80464f4814a41e7161256a7b064d78aae8c6be8
Buzivskoy Vyacheslav <vyacheslav.buzivskoy@ukmig.com.ua>
2020-03-16 10:27:04 +0200

    Add test for ini files and fixtures with ini files

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,9 +1,9 @@
 import genDiff from '../src';
 
-test('Plain JSON genDiff test', () => {
-  const pathToBeforeFile = '__tests__/fixtures/before.json';
-  const pathToAfterFile = '__tests__/fixtures/after.json';
-  const diff = `{
+let diff;
+
+beforeAll(() => {
+  diff = `{
     host: hexlet.io
   + timeout: 20
   - timeout: 50
@@ -11,7 +11,10 @@ test('Plain JSON genDiff test', () => {
   + verbose: true
   - follow: false
 }`;
-
+});
+test('Plain JSON genDiff test', () => {
+  const pathToBeforeFile = '__tests__/fixtures/before.json';
+  const pathToAfterFile = '__tests__/fixtures/after.json';
   expect(new Set(genDiff(pathToBeforeFile, pathToAfterFile).split('\n'))).toEqual(new Set(diff.split('\n')));
   expect((() => {
     const diff = genDiff(pathToBeforeFile, pathToAfterFile).split('\n');
@@ -21,19 +24,21 @@ test('Plain JSON genDiff test', () => {
   })()).toEqual(1);
 });
 
-
 test('Plain YAML genDiff test', () => {
   const pathToBeforeFile = '__tests__/fixtures/before.yaml';
   const pathToAfterFile = '__tests__/fixtures/after.yaml';
-  const diff = `{
-    host: hexlet.io
-  + timeout: 20
-  - timeout: 50
-  - proxy: 123.234.53.22
-  + verbose: true
-  - follow: false
-}`;
+  expect(new Set(genDiff(pathToBeforeFile, pathToAfterFile).split('\n'))).toEqual(new Set(diff.split('\n')));
+  expect((() => {
+    const diff = genDiff(pathToBeforeFile, pathToAfterFile).split('\n');
+    const plusTimeoutKeyIndex = diff.indexOf('  + timeout: 20');
+    const minusTimeoutKeyIndex = diff.indexOf('  - timeout: 50');
+    return Math.abs(plusTimeoutKeyIndex - minusTimeoutKeyIndex);
+  })()).toEqual(1);
+});
 
+test('Plain INI genDiff test', () => {
+  const pathToBeforeFile = '__tests__/fixtures/before.ini';
+  const pathToAfterFile = '__tests__/fixtures/after.ini';
   expect(new Set(genDiff(pathToBeforeFile, pathToAfterFile).split('\n'))).toEqual(new Set(diff.split('\n')));
   expect((() => {
     const diff = genDiff(pathToBeforeFile, pathToAfterFile).split('\n');
