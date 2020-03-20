2afe5ad872198065a603cc48a9e0c703532a6ada
Buzivskoy Vyacheslav <vyacheslav.buzivskoy@ukmig.com.ua>
2020-03-20 12:12:51 +0200

    Add tests for plain output format

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,14 +1,11 @@
 import fs from 'fs';
-import { genDiff, showDiff } from '../src';
+import genDiff from '../src';
 
 describe('gendiff tests', () => {
-  let complexDiffFile;
-  let complexDiffSet;
   let makeDiffSet;
   let getDistBetweenKeys;
 
   beforeAll(() => {
-    complexDiffFile = fs.readFileSync('__fixtures__/complex_diff.txt', 'utf8');
     makeDiffSet = (rawDiff) => new Set(rawDiff.split('\n'));
     getDistBetweenKeys = (rawDiff, key1diff, key2diff) => {
       const splittedDiff = rawDiff.split('\n');
@@ -16,61 +13,101 @@ describe('gendiff tests', () => {
       const key2Index = splittedDiff.indexOf(key2diff);
       return Math.abs(key1Index - key2Index);
     };
-
-    complexDiffSet = makeDiffSet(complexDiffFile);
   });
 
-  test('Complex JSON file content test', () => {
-    const pathToBeforeFile = '__fixtures__/before_complex.json';
-    const pathToAfterFile = '__fixtures__/after_complex.json';
-    expect(makeDiffSet(showDiff(genDiff(pathToBeforeFile, pathToAfterFile)))).toEqual(complexDiffSet);
+  describe('Complex output format tests', () => {
+    let complexDiffFile;
+    let complexDiffSet;
+    let format;
+
+    beforeAll(() => {
+      complexDiffFile = fs.readFileSync('__fixtures__/complex_diff.txt', 'utf8');
+      complexDiffSet = makeDiffSet(complexDiffFile);
+      format = 'complex';
+    });
+
+    test('JSON file content test', () => {
+      const pathToBeforeFile = '__fixtures__/before_complex.json';
+      const pathToAfterFile = '__fixtures__/after_complex.json';
+      expect(makeDiffSet(genDiff(pathToBeforeFile, pathToAfterFile, format))).toEqual(complexDiffSet);
+    });
+
+    test('JSON file changed value test (2 recordes must be beside)', () => {
+      const pathToBeforeFile = '__fixtures__/before_complex.json';
+      const pathToAfterFile = '__fixtures__/after_complex.json';
+      expect(
+        getDistBetweenKeys(
+          genDiff(pathToBeforeFile, pathToAfterFile, format),
+          '      + baz: bars',
+          '      - baz: bas'
+        )
+      ).toEqual(1);
+    });
+
+    test('YAML file content test', () => {
+      const pathToBeforeFile = '__fixtures__/before_complex.yaml';
+      const pathToAfterFile = '__fixtures__/after_complex.yaml';
+      expect(makeDiffSet(genDiff(pathToBeforeFile, pathToAfterFile, format))).toEqual(complexDiffSet);
+    });
+
+    test('YAML file changed value test (2 recordes must be beside)', () => {
+      const pathToBeforeFile = '__fixtures__/before_complex.yaml';
+      const pathToAfterFile = '__fixtures__/after_complex.yaml';
+      expect(
+        getDistBetweenKeys(
+          genDiff(pathToBeforeFile, pathToAfterFile, format),
+          '      + baz: bars',
+          '      - baz: bas'
+        )
+      ).toEqual(1);
+    });
+
+    test('INI file content test', () => {
+      const pathToBeforeFile = '__fixtures__/before_complex.ini';
+      const pathToAfterFile = '__fixtures__/after_complex.ini';
+      expect(makeDiffSet(genDiff(pathToBeforeFile, pathToAfterFile, format))).toEqual(complexDiffSet);
+    });
+
+    test('INI file changed value test (2 recordes must be beside)', () => {
+      const pathToBeforeFile = '__fixtures__/before_complex.ini';
+      const pathToAfterFile = '__fixtures__/after_complex.ini';
+      expect(
+        getDistBetweenKeys(
+          genDiff(pathToBeforeFile, pathToAfterFile, format),
+          '      + baz: bars',
+          '      - baz: bas'
+        )
+      ).toEqual(1);
+    });
   });
 
-  test('Complex JSON file changed value test (2 recordes must be beside)', () => {
-    const pathToBeforeFile = '__fixtures__/before_complex.json';
-    const pathToAfterFile = '__fixtures__/after_complex.json';
-    expect(
-      getDistBetweenKeys(
-        showDiff(genDiff(pathToBeforeFile, pathToAfterFile)),
-        '      + baz: bars',
-        '      - baz: bas'
-      )
-    ).toEqual(1);
-  });
+  describe('Plain output format tests', () => {
+    let plainDiffFile;
+    let plainDiffSet;
+    let format;
 
-  test('Complex YAML file content test', () => {
-    const pathToBeforeFile = '__fixtures__/before_complex.yaml';
-    const pathToAfterFile = '__fixtures__/after_complex.yaml';
-    expect(makeDiffSet(showDiff(genDiff(pathToBeforeFile, pathToAfterFile)))).toEqual(complexDiffSet);
-  });
+    beforeAll(() => {
+      plainDiffFile = fs.readFileSync('__fixtures__/plain_diff.txt', 'utf8');
+      plainDiffSet = makeDiffSet(plainDiffFile);
+      format = 'plain';
+    });
 
-  test('Complex YAML file changed value test (2 recordes must be beside)', () => {
-    const pathToBeforeFile = '__fixtures__/before_complex.yaml';
-    const pathToAfterFile = '__fixtures__/after_complex.yaml';
-    expect(
-      getDistBetweenKeys(
-        showDiff(genDiff(pathToBeforeFile, pathToAfterFile)),
-        '      + baz: bars',
-        '      - baz: bas'
-      )
-    ).toEqual(1);
-  });
+    test('JSON file content test', () => {
+      const pathToBeforeFile = '__fixtures__/before_complex.json';
+      const pathToAfterFile = '__fixtures__/after_complex.json';
+      expect(makeDiffSet(genDiff(pathToBeforeFile, pathToAfterFile, format))).toEqual(plainDiffSet);
+    });
 
-  test('Complex INI file content test', () => {
-    const pathToBeforeFile = '__fixtures__/before_complex.ini';
-    const pathToAfterFile = '__fixtures__/after_complex.ini';
-    expect(makeDiffSet(showDiff(genDiff(pathToBeforeFile, pathToAfterFile)))).toEqual(complexDiffSet);
-  });
+    test('YAML file content test', () => {
+      const pathToBeforeFile = '__fixtures__/before_complex.yaml';
+      const pathToAfterFile = '__fixtures__/after_complex.yaml';
+      expect(makeDiffSet(genDiff(pathToBeforeFile, pathToAfterFile, format))).toEqual(plainDiffSet);
+    });
 
-  test('Complex INI file changed value test (2 recordes must be beside)', () => {
-    const pathToBeforeFile = '__fixtures__/before_complex.ini';
-    const pathToAfterFile = '__fixtures__/after_complex.ini';
-    expect(
-      getDistBetweenKeys(
-        showDiff(genDiff(pathToBeforeFile, pathToAfterFile)),
-        '      + baz: bars',
-        '      - baz: bas'
-      )
-    ).toEqual(1);
+    test('INI file content test', () => {
+      const pathToBeforeFile = '__fixtures__/before_complex.ini';
+      const pathToAfterFile = '__fixtures__/after_complex.ini';
+      expect(makeDiffSet(genDiff(pathToBeforeFile, pathToAfterFile, format))).toEqual(plainDiffSet);
+    });
   });
 });
