a56a5e822ca8cadd3f8857164be2695fd7eef2de
Buzivskoy Vyacheslav <vyacheslav.buzivskoy@ukmig.com.ua>
2020-03-20 15:57:25 +0200

    Update 'gendiff.js' to support format types

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,20 +1,19 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander';
-import { genDiff, showDiff } from '../';
+import genDiff from '../';
 
 
 const program = new Command();
 program
-  .version('0.3.0')
+  .version('1.0.0')
   .usage('gendiff [options] <firstConfig> <secondConfig>')
   .description('Compares two configuration files and shows a difference.')
   .arguments('<firstConfig> <secondConfig>')
-  .option('-f, --format [type]', 'output format')
-  .action((firstConfig, secondConfig) => {
-    const beforeConfig = firstConfig;
-    const afterConfig = secondConfig;
-    console.log(showDiff(genDiff(beforeConfig, afterConfig)));
+  .option('-f, --format [type]', 'output format', 'complex')
+  .action((beforeConfig, afterConfig, cmdObj) => {
+    const fromat = cmdObj.format;
+    console.log(genDiff(beforeConfig, afterConfig, fromat));
   });
 
 program.parse(process.argv);
