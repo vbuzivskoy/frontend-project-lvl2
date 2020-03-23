0dbde6c274659055a4d9b13d0caf15ac975b3be0
Buzivskoy Vyacheslav <vyacheslav.buzivskoy@ukmig.com.ua>
2020-03-23 13:03:08 +0200

    Update npm package version to 1.1.1

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -6,7 +6,7 @@ import genDiff from '../';
 
 const program = new Command();
 program
-  .version('1.0.0')
+  .version('1.1.1')
   .usage('gendiff [options] <firstConfig> <secondConfig>')
   .description('Compares two configuration files and shows a difference.')
   .arguments('<firstConfig> <secondConfig>')
