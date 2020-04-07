2f0523fb5398a51c7fbbe4d782408a9a21d1aba6
Buzivskoy Vyacheslav <vyacheslav.buzivskoy@ukmig.com.ua>
2020-04-07 11:08:48 +0300

    Refactoring of tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,35 +1,36 @@
 import path from 'path';
 import fs from 'fs';
 import genDiff from '../src';
-//import { stringify } from 'querystring';
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFixture = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
-const makeJSONOutputTest = (pathToBeforeFile, pathToAfterFile, format) => {
-  const stringifiedJSON = JSON.parse(genDiff(pathToBeforeFile, pathToAfterFile, format));
-  return stringifiedJSON;
-};
-const parseJSONfixture = (filename) => JSON.parse(readFixture(filename));
+const getDiffToTest = (diff, ouputFormatType) => (
+  ouputFormatType === 'json' ? JSON.parse(diff) : diff
+);
 
 describe('gendiff tests', () => {
   const testData = [
-    ['JSON', 'complex', genDiff, 'before_complex.json', 'after_complex.json', 'complex_diff.txt', readFixture],
-    ['YAML', 'complex', genDiff, 'before_complex.yaml', 'after_complex.yaml', 'complex_diff.txt', readFixture],
-    ['INI', 'complex', genDiff, 'before_complex.ini', 'after_complex.ini', 'complex_diff.txt', readFixture],
-    ['JSON', 'plain', genDiff, 'before_complex.json', 'after_complex.json', 'plain_diff.txt', readFixture],
-    ['YAML', 'plain', genDiff, 'before_complex.yaml', 'after_complex.yaml', 'plain_diff.txt', readFixture],
-    ['INI', 'plain', genDiff, 'before_complex.ini', 'after_complex.ini', 'plain_diff.txt', readFixture],
-    ['JSON', 'json', makeJSONOutputTest, 'before_complex.json', 'after_complex.json', 'diff.json', parseJSONfixture],
-    ['YAML', 'json', makeJSONOutputTest, 'before_complex.yaml', 'after_complex.yaml', 'diff.json', parseJSONfixture],
-    ['INI', 'json', makeJSONOutputTest, 'before_complex.ini', 'after_complex.ini', 'diff.json', parseJSONfixture]
+    ['json', 'complex'],
+    ['yaml', 'complex'],
+    ['ini', 'complex'],
+    ['json', 'plain'],
+    ['yaml', 'plain'],
+    ['ini', 'plain'],
+    ['json', 'json'],
+    ['yaml', 'json'],
+    ['ini', 'json']
   ];
 
   test.each(testData)(
     '%s file content test with %s output format type',
-    (filesFormat, ouputFormatType, makeTest, beforeFileName, afterFileName, expectedDiffFileName, getExpectedDiff) => {
-      const pathToBeforeFile = getFixturePath(beforeFileName);
-      const pathToAfterFile = getFixturePath(afterFileName);
-      const expectedDiff = getExpectedDiff(expectedDiffFileName);
-      expect(makeTest(pathToBeforeFile, pathToAfterFile, ouputFormatType)).toEqual(expectedDiff);
+    (filesFormat, ouputFormatType) => {
+      const firstConfigFilePath = getFixturePath(`firstConfig.${filesFormat}`);
+      const secondConfigFilePath = getFixturePath(`secondConfig.${filesFormat}`);
+      const recievedDiff = genDiff(firstConfigFilePath, secondConfigFilePath, ouputFormatType);
+      const recievedDiffToTest = getDiffToTest(recievedDiff, ouputFormatType);
+      const expectedDiffFilename = `${ouputFormatType}_diff.txt`;
+      const expectedDiff = readFixture(expectedDiffFilename);
+      const expectedDiffToTest = getDiffToTest(expectedDiff, ouputFormatType);
+      expect(recievedDiffToTest).toEqual(expectedDiffToTest);
   });
 });
