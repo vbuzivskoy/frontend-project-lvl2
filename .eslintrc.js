1f4e96b62c5774cddd5a9b5ac94669bbe4ad04d9
Buzivskoy Vyacheslav <vyacheslav.buzivskoy@ukmig.com.ua>
2020-04-15 17:52:30 +0300

    Add linter config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,18 @@
 module.exports = {
-    "env": {
-        "es6": true,
-        "node": true,
-        "jest": true
+    plugins: [
+        'jest',
+        'babel'
+    ],
+    env: {
+        node: true,
+        jest: true
     },
-    "extends": "eslint:recommended",
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
-    },
-    "parserOptions": {
-        "ecmaVersion": 2018,
-        "sourceType": "module"
-    },
-    "rules": {
+    parser: "babel-eslint",
+    extends: [
+        "airbnb-base",
+        "plugin:jest/recommended"
+    ],
+    rules: {
+        'no-console': 0
     }
 };
