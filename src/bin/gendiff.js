817d83752d79f1ed748efad663019ffaafd936ee
Buzivskoy Vyacheslav <vyacheslav.buzivskoy@ukmig.com.ua>
2020-04-21 10:09:13 +0300

    Update version to 2.0.1

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -6,7 +6,7 @@ import genDiff from '..';
 
 const program = new Command();
 program
-  .version('2.0.0')
+  .version('2.0.1')
   .usage('gendiff [options] <firstConfig> <secondConfig>')
   .description('Compares two configuration files and shows a difference.')
   .arguments('<firstConfig> <secondConfig>')
